d0e0426abc6b7fc03b77f6eafc9deb00daa5cfcf
Brad Martin <brma7253@colorado.edu>
2020-05-07 12:31:02 -0600

    take 2, adding SSM param on new branch

--- example.tf
+++ example.tf
@@ -6,5 +6,5 @@ provider "aws" {
 resource "aws_ssm_parameter" "param_from_cloud_v2" {
   name  = "param_from_tf_cloud_v2_200507"
   type  = "String"
-  value = "Hello from Terraform Cloud during 5/7 demo!"
+  value = "Hello from Terraform Cloud during 5/7 demo, take 2!"
 }
